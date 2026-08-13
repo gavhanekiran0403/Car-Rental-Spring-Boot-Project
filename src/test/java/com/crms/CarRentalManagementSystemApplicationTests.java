d2f5ca5249e84c75d4216a0c1a4f347c5604227e
Kiran Gavhane <gavhanekiran0403@gmail.com>
2026-08-13 11:12:22 +0530

    update  CarRentalManagementSystemApplicationTests.java

--- src/test/java/com/crms/CarRentalManagementSystemApplicationTests.java
+++ src/test/java/com/crms/CarRentalManagementSystemApplicationTests.java
@@ -3,7 +3,9 @@ package com.crms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	    "spring.kafka.listener.auto-startup=false"
+	})
 class CarRentalManagementSystemApplicationTests {
 
 	@Test
